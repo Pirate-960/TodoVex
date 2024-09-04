51ee911a7dfe470d353aeb18eaccfd22bb8a766d
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-04 22:57:09 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>TodoVex</h1>
+      <Button variant={"destructive"}> Hi! </Button>
     </main>
   );
 }
