927f42f99c88bf4b5fd5d9b198b65ffcea88a17b
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-04 23:31:30 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>TodoVex</h1>
-      <Button variant={"destructive"}> Hi! </Button>
+      <Button> Hi! </Button>
     </main>
   );
 }
