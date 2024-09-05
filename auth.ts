2cd59238f2ee5192d1124f5a56db767e50e4f40d
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-06 01:01:40 +0300

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,7 +1,13 @@
 import NextAuth from "next-auth"
- 
+import GoogleProvider from "next-auth/providers/google"
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     // Add your providers -> Google!
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        authorization: { params: { prompt: "consent" } },
+    }),
   ],
 })
