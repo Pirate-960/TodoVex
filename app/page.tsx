7fa7fa17738165ea7ba5678afd82c3f335e22099
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-05 12:42:09 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
 import Image from "next/image";
 
 export default function Home() {
+  const tasks = useQuery(api.tasks.get);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>TodoVex</h1>
       <Button> Hi! </Button>
+      <p>Tasks</p>
+      {tasks?.map((task, idx) => <p key = {idx}>{JSON.stringify(task)}</p>)}
     </main>
   );
 }
