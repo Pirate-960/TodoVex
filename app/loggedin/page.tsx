7634df3cd86add436db65ddafa610b0705169c64
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-07 00:38:58 +0300

    Update page.tsx

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -6,10 +6,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>TodoVex</h1>
       <UserProfile/>   
       <Tasks/>
-    </div>
+    </main>
   );
 }
