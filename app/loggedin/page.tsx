56c5edca9cc0c4edc88b3306f9eeaecc5e5c74d3
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-06 19:09:52 +0300

    Update page.tsx

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,13 +1,15 @@
 import Tasks from "@/components/todovex/tasks";
+import UserProfile from "@/components/todovex/user-profile";
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>TodoVex</h1>     
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>TodoVex</h1>
+      <UserProfile/>   
       <Tasks/>
-    </main>
+    </div>
   );
 }
