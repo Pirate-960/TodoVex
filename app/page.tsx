95058036462c0b2eaffb2b149741079f65441ca7
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-06 18:39:33 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
+import { signInAction } from "@/actions/auth-action";
 import Tasks from "@/components/todovex/tasks";
 import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>TodoVex</h1>
-      <Button> Hi! </Button>
-      <Tasks/>
+      <h1>Login</h1>
+      <form action={signInAction}>
+        <Button> Log in </Button>
+      </form>
     </main>
   );
 }
