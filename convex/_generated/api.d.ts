3bb7d57b97d25921523d5db44f1fbc94f1c99cb2
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-06 23:58:45 +0300

    Update api.d.ts

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as authAdapter from "../authAdapter.js";
+import type * as http from "../http.js";
 import type * as tasks from "../tasks.js";
 
 /**
@@ -26,6 +27,7 @@ import type * as tasks from "../tasks.js";
  */
 declare const fullApi: ApiFromModules<{
   authAdapter: typeof authAdapter;
+  http: typeof http;
   tasks: typeof tasks;
 }>;
 export declare const api: FilterApi<
