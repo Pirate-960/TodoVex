367ac9ad37c8fd873c7fbe1c5e5cd0bc6a507707
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-06 23:58:56 +0300

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,44 @@
 "use client";
-
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ReactNode, useMemo } from "react";
+import { ConvexProviderWithAuth, ConvexReactClient } from "convex/react";
 import { Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { ReactNode } from "react";
+import { SessionProvider, useSession } from "next-auth/react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-export function Providers({
+function convexTokenFromSession(session: Session | null): string | null {
+  return session?.convexToken ?? null;
+}
+
+function useAuth() {
+  const { data: session, update } = useSession();
+
+  const convexToken = convexTokenFromSession(session);
+  return useMemo(
+    () => ({
+      isLoading: false,
+      isAuthenticated: session !== null,
+      fetchAccessToken: async ({
+        forceRefreshToken,
+      }: {
+        forceRefreshToken: boolean;
+      }) => {
+        if (forceRefreshToken) {
+          const session = await update();
+
+          return convexTokenFromSession(session);
+        }
+        return convexToken;
+      },
+    }),
+    // We only care about the user changes, and don't want to
+    // bust the memo when we fetch a new token.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [JSON.stringify(session?.user)]
+  );
+}
+
+export default function Providers({
   children,
   session,
 }: {
@@ -16,7 +47,9 @@ export function Providers({
 }) {
   return (
     <SessionProvider session={session}>
-      <ConvexProvider client={convex}>{children}</ConvexProvider>
+      <ConvexProviderWithAuth client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithAuth>
     </SessionProvider>
   );
 }
