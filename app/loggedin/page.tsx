d293540fb079c2ad74e0b9fc4a04df2194d598a2
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-07 18:39:24 +0300

    Update page.tsx

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -7,11 +7,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <SideBar/>
-      <h1>TodoVex</h1>
-      <UserProfile/>   
-      <Tasks/>
-    </main>
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <SideBar />
+      <div className="flex flex-col">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8">
+          <h1>TodoVex</h1>
+          <UserProfile />
+          <Tasks />
+        </main>
+      </div>
+    </div>
   );
 }
