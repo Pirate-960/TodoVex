4a9adc2de08b48c4ddf1ec0bbab9806db61bf38a
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-08 19:41:13 +0300

    Create todo-list.tsx

---
+++ components/todos/todo-list.tsx
@@ -0,0 +1,10 @@
+export default function TodoList() {
+  return (
+    <div className="xl:px-40">
+      {" "}
+      <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8">
+        <h1>TodoVex</h1>
+      </main>
+    </div>
+  );
+}
