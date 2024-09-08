dc34ed680480c22945c4a9625d363f14fe06e504
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-08 13:58:12 +0300

    Update side-bar.tsx

--- components/nav/side-bar.tsx
+++ components/nav/side-bar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import {
   Bell,
+  Calendar,
+  CalendarDays,
   CircleUser,
+  Grid2X2,
   Home,
+  Inbox,
   LineChart,
   Menu,
   Package,
@@ -36,6 +40,23 @@ export const description =
   "A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation links, and a card with a call to action. The main content area shows an empty state with a call to action.";
 
 export default function SideBar() {
+  const primaryNavItems = [
+    {
+      name: "Inbox",
+      link: "/loggedin",
+      icon: <Inbox className="w-4 h-4"/>,
+    },
+    {
+      name: "Today",
+      link: "/loggedin/today",
+      icon: <Calendar className="w-4 h-4"/>,
+    },
+    {
+      name: "Filters & Labels",
+      link: "/loggedin/filter-labels",
+      icon: <Grid2X2 className="w-4 h-4"/>,
+    },
+  ]
   return (
     <div className="hidden border-r bg-muted/40 md:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
@@ -44,41 +65,16 @@ export default function SideBar() {
         </div>
         <div className="flex-1">
           <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-              <Home className="h-4 w-4" />
-              Dashboard
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-              <ShoppingCart className="h-4 w-4" />
-              Orders
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-            >
-              <Package className="h-4 w-4" />
-              Products{" "}
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-              <Users className="h-4 w-4" />
-              Customers
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-              <LineChart className="h-4 w-4" />
-              Analytics
-            </Link>
+            {primaryNavItems.map((Item, idx) => (
+              <Link
+                key={idx}
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Home className="h-4 w-4" />
+                Dashboard
+              </Link>
+            ))}
           </nav>
         </div>
         <div className="mt-auto p-4">
