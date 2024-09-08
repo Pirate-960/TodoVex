e961b3fb75104127ec7f592c81c5bca50fe5cc2c
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-08 18:22:52 +0300

    Update auth-action.ts

--- actions/auth-action.ts
+++ actions/auth-action.ts
@@ -1,8 +1,12 @@
-'use server'
+"use server";
 
-import { signIn } from "@/auth"
+import { signIn, signOut } from "@/auth"
 
 
 export async function signInAction() {
     await signIn("google", { redirectTo: "/loggedin"});
+}
+
+export async function signOutAction() {
+    await signOut({ redirectTo: "/"});
 }
