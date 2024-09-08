1d50bf46592cc3c7c286f00e90f8cd4e8135d8b3
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-08 20:53:35 +0300

    Update todo-list.tsx

--- components/todos/todo-list.tsx
+++ components/todos/todo-list.tsx
@@ -1,10 +1,9 @@
 export default function TodoList() {
   return (
     <div className="xl:px-40">
-      {" "}
-      <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8">
-        <h1>TodoVex</h1>
-      </main>
+        <div className="flex items-center justify-between">
+            
+        </div>
     </div>
   );
 }
