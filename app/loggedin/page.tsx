aa1a81c9deee71ab0f282d72189855190aac35ae
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-08 12:31:00 +0300

    Update page.tsx

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,3 +1,4 @@
+import MobileNav from "@/components/nav/mobile-nav";
 import SideBar from "@/components/nav/side-bar";
 import Tasks from "@/components/todovex/tasks";
 import UserProfile from "@/components/todovex/user-profile";
@@ -10,6 +11,7 @@ export default function Home() {
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <SideBar />
       <div className="flex flex-col">
+        <MobileNav />
         <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8">
           <h1>TodoVex</h1>
           <UserProfile />
