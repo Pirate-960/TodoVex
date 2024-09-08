de87a7b41f4dd00395609fd2dbaba75df2844a6f
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-08 17:49:51 +0300

    Update user-profile.tsx

--- components/nav/user-profile.tsx
+++ components/nav/user-profile.tsx
@@ -3,6 +3,12 @@
 import { useSession } from "next-auth/react";
 import React from "react";
 import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 export default function UserProfile() {
   const session = useSession();
@@ -12,20 +18,24 @@ export default function UserProfile() {
   const email = session?.data?.user?.email;
 
   return (
-    <div
-      className="flex items-center justify-start gap-1 
-    lg:gap-2 m-0 p-0 lg:px-3 lg:w-full bg-white"
-    >
-      {imageUrl && (
-        <Image
-          src={imageUrl}
-          width={24}
-          height={24}
-          alt={`${name} profile picture`}
-          className="rounded-full"
-        />
-      )}
-      <p className="truncate">{email}</p>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div className="flex items-center justify-start gap-1 lg:gap-2 m-0 p-0 lg:px-3 lg:w-full bg-white">
+          {imageUrl && (
+            <Image
+              src={imageUrl}
+              width={24}
+              height={24}
+              alt={`${name} profile picture`}
+              className="rounded-full"
+            />
+          )}
+          <p className="truncate">{email}</p>
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuItem className="lg:w-full px-28">log out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
