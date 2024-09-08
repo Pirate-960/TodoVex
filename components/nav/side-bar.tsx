bdf750858f78119c8217e5865de8fef07fd1b1c9
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-08 15:03:02 +0300

    Update side-bar.tsx

--- components/nav/side-bar.tsx
+++ components/nav/side-bar.tsx
@@ -34,29 +34,13 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import UserProfile from "../todovex/user-profile";
+import UserProfile from "./user-profile";
+import { primaryNavItems } from "@/utils";
 
 export const description =
   "A products dashboard with a sidebar navigation and a main content area. The dashboard has a header with a search input and a user menu. The sidebar has a logo, navigation links, and a card with a call to action. The main content area shows an empty state with a call to action.";
 
 export default function SideBar() {
-  const primaryNavItems = [
-    {
-      name: "Inbox",
-      link: "/loggedin",
-      icon: <Inbox className="w-4 h-4"/>,
-    },
-    {
-      name: "Today",
-      link: "/loggedin/today",
-      icon: <Calendar className="w-4 h-4"/>,
-    },
-    {
-      name: "Filters & Labels",
-      link: "/loggedin/filter-labels",
-      icon: <Grid2X2 className="w-4 h-4"/>,
-    },
-  ]
   return (
     <div className="hidden border-r bg-muted/40 md:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
@@ -65,14 +49,14 @@ export default function SideBar() {
         </div>
         <div className="flex-1">
           <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-            {primaryNavItems.map((Item, idx) => (
+            {primaryNavItems.map(({name, icon, link}, idx) => (
               <Link
                 key={idx}
-                href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                href={link}
+                className="flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary"
               >
-                <Home className="h-4 w-4" />
-                Dashboard
+                {icon}
+                {name}
               </Link>
             ))}
           </nav>
