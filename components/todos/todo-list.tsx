e59de80bc9304dc9fba7663cdaf6d583c5dbfeaa
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-09 20:41:47 +0300

    Update todo-list.tsx

--- components/todos/todo-list.tsx
+++ components/todos/todo-list.tsx
@@ -2,8 +2,9 @@ export default function TodoList() {
   return (
     <div className="xl:px-40">
         <div className="flex items-center justify-between">
-            
+            <h1 className="text-lg font-semibold md:text-2xl">Inbox</h1>
         </div>
+        <div className="flex flex-col gap-1 py-4">todos</div>
     </div>
   );
 }
