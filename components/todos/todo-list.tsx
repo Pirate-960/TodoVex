544aed51338476c7a2569842c35e7adb118a9a10
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-11 12:28:57 +0300

    Update todo-list.tsx

--- components/todos/todo-list.tsx
+++ components/todos/todo-list.tsx
@@ -1,13 +1,24 @@
 "use client";
-
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
 import { Checkbox } from "../ui/checkbox";
+import Task from "./task";
+import { CircleCheckBig } from "lucide-react";
+import Todos from "./todos";
+import CompletedTodos from "./completed-todos";
+import { AddTaskWrapper } from "../add-tasks/add-task-button";
 
 export default function TodoList() {
   const todos = useQuery(api.todos.get) ?? [];
+  const completedTodos = useQuery(api.todos.completedTodos) ?? [];
+  const inCompleteTodos = useQuery(api.todos.inCompleteTodos) ?? [];
+  const totalTodos = useQuery(api.todos.totalTodos) ?? 0;
 
-  if (todos === undefined) {
+  if (
+    todos === undefined ||
+    completedTodos === undefined ||
+    inCompleteTodos === undefined
+  ) {
     <p>Loading...</p>;
   }
   return (
@@ -16,20 +27,13 @@ export default function TodoList() {
         <h1 className="text-lg font-semibold md:text-2xl">Inbox</h1>
       </div>
       <div className="flex flex-col gap-1 py-4">
-        <div className="flex items-center space-x-2">
-          {todos.map(({ taskName }, idx) => ( 
-          <div key={idx}>
-            <Checkbox id="todo" />
-          <label
-            htmlFor="todo"
-            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-          >
-            {taskName}
-          </label>
-          </div>
-          ))}
-        </div>
+        <Todos items={inCompleteTodos} />
       </div>
+      <AddTaskWrapper />
+      <div className="flex flex-col gap-1 py-4">
+        <Todos items={completedTodos} />
+      </div>
+      <CompletedTodos totalTodos={totalTodos} />
     </div>
   );
 }
