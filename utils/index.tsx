af25722b1a5d6a4753c11c768f51b0958af989c1
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-11 12:23:28 +0300

    Update index.tsx

--- utils/index.tsx
+++ utils/index.tsx
@@ -2,6 +2,7 @@ import { Calendar, CalendarDays, Grid2X2, Inbox } from "lucide-react";
 
 export const primaryNavItems = [
   {
+    id: "primary",
     name: "Inbox",
     link: "/loggedin",
     icon: <Inbox className="w-4 h-4" />,
@@ -17,8 +18,11 @@ export const primaryNavItems = [
     icon: <CalendarDays className="w-4 h-4" />,
   },
   {
+    id: "filters",
     name: "Filters & Labels",
     link: "/loggedin/filter-labels",
     icon: <Grid2X2 className="w-4 h-4" />,
   },
 ];
+
+export const GET_STARTED_PROJECT_ID = "k97fs8npdxzkr39y5vjcp9kq1d6tycm1";
