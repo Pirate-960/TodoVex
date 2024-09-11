4ec3dc5c111dd298754fa604384625ea483de087
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-09-11 12:42:59 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,26 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans_Georgian } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ["latin"] });
+const defaultFont = Noto_Sans_Georgian({ subsets: ["latin"] });
+
+const ORIGIN_URL =
+  process.env.NODE === "production"
+    ? "https://todovex.ai"
+    : "http://localhost:3000";
 
 export const metadata: Metadata = {
-  title: "TodoVex.ai",
-  description: "An Open Source AI-Powered Todoist Application",
+  title: "Todovex.ai",
+  description:
+    "TodoVex seamlessly organizes your tasks and predicts what's nextusing AI.",
+  icons: {
+    icon: "/icon.ico",
+  },
+  metadataBase: new URL(ORIGIN_URL),
+  alternates: {
+    canonical: ORIGIN_URL,
+  },
 };
 
 export default function RootLayout({
@@ -16,8 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={defaultFont.className}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
